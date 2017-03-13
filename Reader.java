48a2a19691a5bb040832ab76cf35db1f8e8c8164
Soronie <roarroyo@ucdavis.edu>
2017-03-12 18:20:57 -0700

    change int to double, add test files

--- Reader.java
+++ Reader.java
@@ -7,17 +7,25 @@ public class Reader{
 
 	final static int HEIGHT = 128;
 	final static int WIDTH = 120;
+
+	private double sigmoidFunction(int x)
+	{
+		return (1/(1+Math.pow(Math.E,-x)));
+	}
+
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		String str;
-		File[] maleFiles, femaleFiles;
-		File m = null, f = null;
+		File[] maleFiles, femaleFiles, testFiles;
+		File m = null, f = null, t = null;
 		ArrayList<String> allFiles = new ArrayList<String>();
 
 		m = new File("./Male/");
 		maleFiles = m.listFiles();
 		f = new File("./Female/");
 		femaleFiles = f.listFiles();
+		t = new File("./Test/");
+		testFiles = t.listFiles();
 
 		for(File i : maleFiles)
 		{
@@ -39,9 +47,16 @@ public class Reader{
 		  }
 		}
 
-		List<List<Integer>> pixels = new ArrayList<List<Integer>>(HEIGHT);
+		for(File i : testFiles)
+		{
+		  str = ("./Test/");
+		  str = str + i.getName();
+		  allFiles.add(str);
+		}
+
+		List<List<Double>> pixels = new ArrayList<List<Double>>(HEIGHT);
 		for(int i = 0; i < HEIGHT; i++)
-		  pixels.add(new ArrayList<Integer>(WIDTH));
+		  pixels.add(new ArrayList<Double>(WIDTH));
 		try
 		{
 		  //Testing to see if input file is stored in 2D ArrayList
@@ -49,9 +64,9 @@ public class Reader{
 		  for(int i = 0; i < HEIGHT; i++)
 		  {
 		    for(int j = 0; j < WIDTH; j++)
-		      if(fileScanner.hasNextInt())
+		      if(fileScanner.hasNextDouble())
 		      {
-			pixels.get(i).add(fileScanner.nextInt());
+			pixels.get(i).add(fileScanner.nextDouble());
 			System.out.print(pixels.get(i).get(j) + " ");
 		      }
 		    System.out.println();
